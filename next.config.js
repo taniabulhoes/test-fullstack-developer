050d2ca7fbd89f0449df93a70e8efc7cd6cac6cf
Diego Gomes <contato@diegodg.com>
2023-03-27 09:11:26 -0300

    chore: removes development code

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // redirects: async () => {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/signin",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
