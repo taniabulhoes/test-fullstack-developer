a233ac21070835ea4f4f6387e05db2407a3d940f
cauerrds <caue_rrds@hotmail.com>
2023-07-04 20:21:32 -0300

    build: OAuth

---
+++ frontend/src/components/routeHandler/RouteHandler.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { checkIsPublicRoute } from '@/helpers/checkIsPublic';
+import { usePathname, useRouter } from 'next/navigation';
+import { ReactNode, useEffect, useState } from 'react'
+import { PrivateRoute } from '../privateRoute/PrivateRoute';
+import { checkIsAuthenticated } from '@/helpers/checkUserIsAuthenticated';
+import { setUserToken } from '@/redux/features/tasks/task';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '@/redux/store/store';
+import { APP_ROUTES } from '@/constants/routes';
+
+type RouteHandlerProps = {
+    children: ReactNode
+}
+
+const RouteHandler = ({ children }: RouteHandlerProps) => {
+    const path = usePathname();
+    const { push } = useRouter()
+    const dispatch = useDispatch<AppDispatch>()
+
+
+    const isPublic = checkIsPublicRoute(path)
+
+    useEffect(()=> {
+    const isAuthenticated = checkIsAuthenticated()
+    if(isAuthenticated){
+        dispatch(setUserToken(isAuthenticated))
+        return push(APP_ROUTES.private.home.path)
+    }
+    
+    },[])
+
+    return(
+        <>
+            {isPublic && children}
+            {!isPublic &&  
+                <PrivateRoute>
+                    {children}
+                </PrivateRoute>
+            }
+        </>
+
+    )
+}
+
+export { RouteHandler }
