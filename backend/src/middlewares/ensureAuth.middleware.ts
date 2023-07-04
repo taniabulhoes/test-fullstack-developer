aff806003140dc52ee69cad0cb660dbc5a41b1de
cauerrds <caue_rrds@hotmail.com>
2023-07-04 20:08:49 -0300

    feat: Auth Middleware

---
+++ backend/src/middlewares/ensureAuth.middleware.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import { userModel } from "../models/users.models.js";
+import { ErrorTypes } from "../errors/errorCatalog.js";
+
+const ensureAuthMiddleware = async (request: Request, response: Response, next: NextFunction) => {
+    const authHeader = request.headers.authorization
+
+    if (!authHeader) {
+        throw new Error(ErrorTypes.MissingHeader)
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+        throw new Error(ErrorTypes.tokenNotFound)
+    }
+
+    const secretKey = "jsonwebtoken"
+
+    verify(token, secretKey, (err, decoded) => {
+        if (!decoded) {
+            throw new Error()
+        }
+        const { userId } = <any>decoded;
+        request.body.userId = userId;
+    });
+
+    const user = await userModel.getUserById(request.body.userId)
+    if (!user) {
+        throw new Error(ErrorTypes.NotFound)
+    }
+
+    next()
+}
+
+export { ensureAuthMiddleware }
