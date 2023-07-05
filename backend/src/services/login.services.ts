3efe0c256effeadd64d3e9e8b7327fccbfe249ca
cauerrds <caue_rrds@hotmail.com>
2023-07-05 01:49:39 -0300

    fix: error handler

--- backend/src/services/login.services.ts
+++ backend/src/services/login.services.ts
@@ -1,32 +1,28 @@
+import { config } from "../configs/configs.js";
 import { ErrorTypes } from "../errors/errorCatalog.js";
 import { ILoginRequest } from "../interfaces/ILogin.js"
 import { userModel } from "../models/users.models.js";
 import * as bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 
-const secretKey = "jsonwebtoken"
+const secretKey = config.JWT_KEY
 
 const login = async (userData: ILoginRequest) => {
     if (!userData.password) {
-        //error
-        console.log("error on !userData.name");
+        throw new Error(ErrorTypes.PasswordRequired)
     }
 
     if (!userData.email) {
-        //error
-        console.log("error on !userData.email");
+        throw new Error(ErrorTypes.EmailRequired)
     }
 
     const user = await userModel.getUserByEmail(userData.email)
 
     if (!user) {
-        //error
-        console.log("error !user");
+        throw new Error(ErrorTypes.NotFound)
     }
 
     const passwordMatch = await bcrypt.compare(userData.password, user.password_hash)
-    console.log(user.password_hash);
-    console.log(userData.password);
     if (!passwordMatch) {
         throw new Error(ErrorTypes.UnauthorizedError)
     }
