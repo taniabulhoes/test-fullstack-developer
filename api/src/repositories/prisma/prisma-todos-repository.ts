a7f85f452acd1ff494e901a270e0c97981c182d8
Nailson <nailson.ivs@codens.com.br>
2023-11-15 17:05:57 -0300

    :sparkles: :Feat Create Repository Models Prisma

---
+++ api/src/repositories/prisma/prisma-todos-repository.ts
@@ -0,0 +1,78 @@
+import { Prisma } from '@prisma/client'
+import { IUsersRepository, User, UserCreateInput } from '../i-user-repository'
+import { prisma } from 'src/lib/prisma'
+import { CreateTodoInput, DeleteTodoInput, ITodosRepository, Todo, UpdateTodoInput } from '../i-todo-repository'
+
+class PrismaTodosRepository implements ITodosRepository {
+  async findById(id: string) {
+    const todo = await prisma.todo.findUnique({
+      where: {
+        id
+      }
+    })
+
+    return todo
+  }
+  
+  async findBySubject(subject: string){
+    const todo = await prisma.todo.findFirst({
+      where: {
+        subject
+      }
+    })
+
+    return todo
+  
+  }
+  
+  async list(userId: string, page: number, query?: string){
+    console.log(query)
+    const todos = await prisma.todo.findMany({
+      where: {
+        subject: {
+          contains: query,
+          mode: 'insensitive'
+        }
+      }      
+    })
+
+    return todos
+  }
+ 
+  async create(data: CreateTodoInput){
+    const todo = await prisma.todo.create({
+      data,
+    })
+
+    return todo
+  }
+
+  async update(data: UpdateTodoInput){
+    const todo = await prisma.todo.update({
+      where: {
+        id: data.id,
+        user_id: data.user_id
+      },
+      data: data
+    })
+
+    return todo
+  }
+
+  async delete(data: DeleteTodoInput){
+    console.log(data)
+
+    await prisma.todo.delete({
+      where: {
+        id: data.id,
+        user_id: data.user_id
+      },
+    })
+
+    return null
+  }
+
+
+}
+
+export {PrismaTodosRepository}
---
+++ api/src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,28 @@
+import { Prisma } from '@prisma/client'
+import { IUsersRepository, User, UserCreateInput } from '../i-user-repository'
+import { prisma } from 'src/lib/prisma'
+
+class PrismaUsersRepository implements IUsersRepository {
+  async findByEmail(email: string){
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      }
+    })
+
+    return user
+  }
+ 
+  async create(data: UserCreateInput){
+    console.log(data)
+
+    const user = await prisma.user.create({
+      data
+    })
+
+    return user
+  }
+
+}
+
+export {PrismaUsersRepository}
