e7bee7cdeaa3e00b718a7f4e121539b9a85bbe1f
Stefano Branz <stefano.branz@acct.global>
2024-01-24 14:57:35 -0300

    created pages and components

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      
+      <h1>LOGIN PAGE</h1>
     </main>
   );
 }
---
+++ todo-app/src/app/task-list/page.tsx
@@ -0,0 +1,7 @@
+export default function TaskList() {
+  return (
+    <main>
+      <h1>TASK LIST</h1>
+    </main>
+  );
+}
