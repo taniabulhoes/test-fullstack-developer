54cc7348cad9cc5a82ba26bbeb7b86f2eb39f945
Stefano Branz <stefano.branz@acct.global>
2024-01-28 18:14:13 -0300

    test server deploy 3

--- backend-todo-app/server.js
+++ backend-todo-app/server.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
+app.get('/api/status', (_req, res) => {
+  res.json({ status: 'Server is running' });
+});
+
 const PORT = process.env.PORT || 2001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
